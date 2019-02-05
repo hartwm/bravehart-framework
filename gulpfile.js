b729f9b521b984b02e59ab03d3fcfc3ad333cfe3
Will Hart <whart@davisadagency.com>
2019-02-05 17:07:39 -0500

    imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,13 @@ var babel = require('gulp-babel');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var imagemin = require('gulp-imagemin');
-var gulpSequence = require('gulp-sequence');
+var cssnano = require('gulp-cssnano');
+var terser = require('gulp-terser');
 var browserSync = require('browser-sync')
   .create();
+var gulpSequence = require('gulp-sequence');
 var useref = require('gulp-useref');
-var uglify = require('gulp-terser');
 var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
 var cache = require('gulp-cache');
 var del = require('del');
@@ -36,12 +35,20 @@ gulp.task('browser-sync', function() {
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
-    .pipe(sass()
-      .on('error', sass.logError))
+    .pipe(plumber({
+      errorHandler: function(err) {
+        console.log(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sass({ errLogToConsole: true }))
     .pipe(autoprefixer({
       browsers: ['last 4 versions'],
       cascade: false
     }))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write(undefined, { sourceRoot: null }))
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({
       stream: true
@@ -52,8 +59,9 @@ gulp.task('sass', function() {
 
 gulp.task('js', function() {
   return gulp.src('app/js/*.js') // Gets all files ending with .scss in app/scss
-    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(babel())
+    .pipe(terser())
     .pipe(concat('all.js'))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('app/public'))
@@ -63,44 +71,27 @@ gulp.task('js', function() {
 });
 
 
-// gulp.task('script', () =>
-//   gulp.src('src/**/*.js')
-//   .pipe(sourcemaps.init())
-//   .pipe(babel({
-//     presets: ['@babel/env']
-//   }))f
-//   .pipe(concat('all.js'))
-//   .pipe(sourcemaps.write('.'))
-//   .pipe(gulp.dest('public'))
-// );
+gulp.task('images', function() {
+  return gulp.src('app/img/*')
+    .pipe(imagemin([
+    imagemin.gifsicle({ interlaced: true }),
+    imagemin.jpegtran({ progressive: true }),
+    imagemin.optipng({ optimizationLevel: 5 }),
+    imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+]))
+    .pipe(gulp.dest('app/public/img'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
 
 
-// gulp.task('js', () => {
-//   return gulp.src('src/js/main.js')
-//     .pipe(babel({
-//       presets: ['es2015']
-//     }))
-//     .pipe(gulp.dest('public'));
-// });
 
-// gulp.task('default', ['js'], () => {
+// gulp.task('default', ['js', sass], () => {
 //   gulp.watch('src/js/main.js', ['js']);
-// });
-
-
-
-// gulp.task('sass', function() {
-//   var stream = gulp.src(paths.sass + '/*.scss')
-//     .pipe(plumber({
-//       errorHandler: function(err) {
-//         console.log(err);
-//         this.emit('end');
-//       }
-//     }))
-//     .pipe(sourcemaps.init({ loadMaps: true }))
-//     .pipe(sass({ errLogToConsole: true }))
-//     .pipe(autoprefixer('last 2 versions'))
-//     .pipe(sourcemaps.write(undefined, { sourceRoot: null }))
-//     .pipe(gulp.dest(paths.css))
-//   return stream;
 // });
